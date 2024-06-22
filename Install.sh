0a98eff4299845a251e24f7bc34596b32422e659
coffandro <macrosedk@gmail.com>
2024-06-22 21:14:12 +0200

    remake installer, move config to inside installer

--- Install.sh
+++
@@ -1,7 +0,0 @@
-pkill -f Nautilus
-
-mkdir -p ~/.local/share/nautilus-python/extensions/NautilusApplications
-cp Extension/config.json ~/.local/share/nautilus-python/extensions/NautilusApplications
-cp Extension/NautilusApplications.py ~/.local/share/nautilus-python/extensions/NautilusApplications
-cp Extension/NautliusApplications-runner.py ~/.local/share/nautilus-python/extensions
-nautilus
---
+++ install.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+usage() {
+cat << EOF
+Usage: Nautilus Application Adder [OPTION]...
+
+OPTIONS:
+  -i, --install,         Install Nautilus Application Adder, will kill
+  -r, --remove,          Remove installed themes
+  -c, --clean,           Clean this directory after install
+
+  -h, --help              Show help
+EOF
+}
+
+install() {
+mkdir -p ~/.local/share/nautilus-python/extensions/NautilusApplications
+cp Extension/NautilusApplications.py ~/.local/share/nautilus-python/extensions/NautilusApplications
+cp Extension/NautliusApplications-runner.py ~/.local/share/nautilus-python/extensions
+
+ConfigFile="$HOME/.local/share/nautilus-python/extensions/NautilusApplications/config.json"
+if [ -e $ConfigFile ]; then
+    echo "Config exists, removing"
+    rm $ConfigFile
+fi
+
+touch $ConfigFile
+echo '{' >> $ConfigFile
+echo '    "items": {' >> $ConfigFile
+echo '        "_comment": "Items to include in the context menu",' >>  $ConfigFile
+echo '        "AddToLocal": true' >> $ConfigFile
+echo '    }' >> $ConfigFile
+echo '}' >> $ConfigFile
+}
+
+remove() {
+rm -r ~/.local/share/nautilus-python/extensions/NautilusApplications
+rm ~/.local/share/nautilus-python/extensions/NautliusApplications-runner.py
+}
+
+clean() {
+Directory=${PWD}
+rm -r $Directory
+}
+
+VALID_ARGS=$(getopt -o hirc --long help,install,remove,clean -- "$@")
+if [[ $? -ne 0 ]]; then
+    exit 1;
+fi
+
+eval set -- "$VALID_ARGS"
+while [ : ]; do
+  case "$1" in
+    -h | --help)
+        usage
+        shift
+        ;;
+    -i | --install)
+        install
+        shift
+        ;;
+    -r | --remove)
+        remove
+        shift
+        ;;
+    -c | --clean)
+        clean
+        shift
+        ;;
+    --) shift;
+        break
+        ;;
+  esac
+done
