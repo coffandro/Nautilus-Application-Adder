255c6dff8cbce4153e4e92a4e5baeedd3e551f77
coffandro <macrosedk@gmail.com>
2024-06-22 21:42:12 +0200

    Add python-nautilus installing script

--- install.sh
+++ install.sh
@@ -14,6 +14,7 @@ EOF
 }
 
 install() {
+InstallDeps
 mkdir -p ~/.local/share/nautilus-python/extensions/NautilusApplications
 cp Extension/NautilusApplications.py ~/.local/share/nautilus-python/extensions/NautilusApplications
 cp Extension/NautliusApplications-runner.py ~/.local/share/nautilus-python/extensions
@@ -31,6 +32,51 @@ echo '        "_comment": "Items to include in the context menu",' >>  $ConfigFi
 echo '        "AddToLocal": true' >> $ConfigFile
 echo '    }' >> $ConfigFile
 echo '}' >> $ConfigFile
+}
+
+InstallDeps() {
+# Install python-nautilus
+echo "Installing python-nautilus..."
+if type "pacman" > /dev/null 2>&1
+then
+    # check if already install, else install
+    pacman -Qi python-nautilus &> /dev/null
+    if [ `echo $?` -eq 1 ]
+    then
+        sudo pacman -S --noconfirm python-nautilus
+    else
+        echo "python-nautilus is already installed"
+    fi
+elif type "apt-get" > /dev/null 2>&1
+then
+    # Find Ubuntu python-nautilus package
+    package_name="python-nautilus"
+    found_package=$(apt-cache search --names-only $package_name)
+    if [ -z "$found_package" ]
+    then
+        package_name="python3-nautilus"
+    fi
+
+    # Check if the package needs to be installed and install it
+    installed=$(apt list --installed $package_name -qq 2> /dev/null)
+    if [ -z "$installed" ]
+    then
+        sudo apt-get install -y $package_name
+    else
+        echo "$package_name is already installed."
+    fi
+elif type "dnf" > /dev/null 2>&1
+then
+    installed=`dnf list --installed nautilus-python 2> /dev/null`
+    if [ -z "$installed" ]
+    then
+        sudo dnf install -y nautilus-python
+    else
+        echo "nautilus-python is already installed."
+    fi
+else
+    echo "Failed to find python-nautilus, please install it manually."
+fi
 }
 
 remove() {
